41664a16bfa9ac375369dcd11e0c7a46f6517bb1
user <azatrashov141102@gmail.com>
2022-09-13 18:20:33 +0600

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,19 @@
+while True:
+    print("\nЭто программа калькулятор!!!")
+    a = input("Введите первое число: ")
+    b = input("Введите второе число: ")
+    op = input("Какую операцию хотите сделать? (+, -, *, /): ")
+    if op == "*":
+        e = int(a) * int(b)
+        print(f"Результат: {e}")
+    elif op == "-":
+        f = int(a) - int(b)
+        print(f"Результат: {f}")
+    elif op == "/":
+        g = int(a) / int(b)
+        print(f"Результат: {int(g)}")
+    elif op == "+":
+        c = int(a) + int(b)
+        print(f"Результат: {c}")
+    else:
+        print("Нет такой операции!")
